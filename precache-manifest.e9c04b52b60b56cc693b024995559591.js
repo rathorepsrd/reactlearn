2d1b11dcdd9a63d8cac5cb2b7b50525ebd933cf4
Pooja <rathorepsrd@gmail.com>
2021-05-25 17:26:25 +0530

    Updates

---
+++ precache-manifest.e9c04b52b60b56cc693b024995559591.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37812ecc69879389aed9cc16cee4d152",
+    "url": "/reactlearn/index.html"
+  },
+  {
+    "revision": "3244acf5b0ce4e0b8d71",
+    "url": "/reactlearn/static/css/main.94a0df77.chunk.css"
+  },
+  {
+    "revision": "badeaf8589284ec42708",
+    "url": "/reactlearn/static/js/2.d4c47e2e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/reactlearn/static/js/2.d4c47e2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3244acf5b0ce4e0b8d71",
+    "url": "/reactlearn/static/js/main.f59dbbfb.chunk.js"
+  },
+  {
+    "revision": "18d06b0424b62d7da41a",
+    "url": "/reactlearn/static/js/runtime-main.0dc876ec.js"
+  }
+]);
