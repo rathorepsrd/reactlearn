0d9a9039bac65b2c33202033659a16561394a49b
Pooja <rathorepsrd@gmail.com>
2021-05-25 17:21:19 +0530

    pages git gh-pages online host menubar

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './MainPage.css'
 export default class MainPage extends React.Component {
    render(){
      return (
-        <div>
-          <button onClick={this.props.toggle}>Click me!</button>
-        </div>
+        <div class="container" onClick={this.props.toggle}>
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+      </div>
      )
     }   
 }
